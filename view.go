ef60f38fb04468f5a7c69bc8999bb38cfab69145
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-07-25 23:45:00 -0400

    refactor view.go

--- view.go
+++ view.go
@@ -14,20 +14,6 @@ const (
 	minLineNumMagn = 3
 )
 
-func makeCharGrid(width, height int) [][]string {
-	grid := make([][]string, height)
-
-	for y, _ := range grid {
-		row := make([]string, width)
-		for x, _ := range row {
-			row[x] = " "
-		}
-		grid[y] = row
-	}
-
-	return grid
-}
-
 func (m model) View() tea.View {
 	if m.err != nil {
 		return tea.NewView(fmt.Sprintf("\nWe had some trouble: %v\n\n", m.err))
@@ -45,58 +31,20 @@ func (m model) View() tea.View {
 
 	grid := makeCharGrid(colsToDisplay, linesToDisplay)
 
-	// content
-	for gridY := range linesToDisplay {
-		absY := gridY + m.paneOffsetY
+	grid = renderLines(grid, m.lines, linesToDisplay, colsToDisplay, m.paneOffsetY, m.paneOffsetX)
 
-		if absY >= len(m.lines) {
-			break
-		}
-
-		line := m.lines[absY]
-
-		for gridX := range colsToDisplay {
-			absX := gridX + m.paneOffsetX
-
-			if absX >= len(line) {
-				break
-			}
-
-			grid[gridY][gridX] = string(line[absX])
-		}
-	}
-
-	// header
 	header := getHeader(m.termWidth)
 
-	// status bar
 	statusBar := getStatusBar(m.status, m.termWidth)
 
-	// Send the UI for rendering
-	outLines := make([]string, len(grid))
-	for gridY, chars := range grid {
-		absY := gridY + m.paneOffsetY
-		line := strings.Join(chars, "")
-
-		lineStyle := baseStyle
-		numStyle := lineNumStyle.Width(m.reservedFromLeft)
-
-		if m.cursorY == absY {
-			lineStyle = cursorLineStyle
-			numStyle = numStyle.Foreground(lipgloss.White)
-		}
-
-		numStyle = numStyle.Inherit(lineStyle)
-
-		lineNum := ""
-		if m.showNums && absY < len(m.lines) {
-			lineNum = strconv.Itoa(absY + 1)
-			lineNum = numStyle.Render(lineNum)
-		}
-
-		outLines[gridY] = lineNum + lineStyle.Render(line)
-	}
-	out := strings.Join(outLines, "\n")
+	out := getOutString(
+		grid,
+		m.cursorY,
+		m.paneOffsetY,
+		m.reservedFromLeft,
+		len(m.lines),
+		m.showNums,
+	)
 
 	v := tea.NewView(header + "\n" + out + "\n" + statusBar)
 
@@ -113,4 +61,79 @@ func (m model) View() tea.View {
 	v.AltScreen = true
 
 	return v
+}
+
+func makeCharGrid(width, height int) [][]string {
+	grid := make([][]string, height)
+
+	for y, _ := range grid {
+		row := make([]string, width)
+		for x, _ := range row {
+			row[x] = " "
+		}
+		grid[y] = row
+	}
+
+	return grid
+}
+
+func getOutString(
+	grid [][]string,
+	cursorY, paneOffsetY, reservedFromLeft, lineCount int,
+	showNums bool,
+) string {
+	outLines := make([]string, len(grid))
+
+	for gridY, chars := range grid {
+		absY := gridY + paneOffsetY
+		line := strings.Join(chars, "")
+
+		lineStyle := baseStyle
+		numStyle := lineNumStyle.Width(reservedFromLeft)
+
+		if cursorY == absY {
+			lineStyle = cursorLineStyle
+			numStyle = numStyle.Foreground(lipgloss.White)
+		}
+
+		numStyle = numStyle.Inherit(lineStyle)
+
+		lineNum := ""
+		if showNums && absY < lineCount {
+			lineNum = strconv.Itoa(absY + 1)
+			lineNum = numStyle.Render(lineNum)
+		}
+
+		outLines[gridY] = lineNum + lineStyle.Render(line)
+	}
+
+	return strings.Join(outLines, "\n")
+}
+
+func renderLines(
+	grid [][]string,
+	lines []string,
+	linesToDisplay, colsToDisplay, paneOffsetY, paneOffsetX int,
+) [][]string {
+	for gridY := range linesToDisplay {
+		absY := gridY + paneOffsetY
+
+		if absY >= len(lines) {
+			break
+		}
+
+		line := lines[absY]
+
+		for gridX := range colsToDisplay {
+			absX := gridX + paneOffsetX
+
+			if absX >= len(line) {
+				break
+			}
+
+			grid[gridY][gridX] = string(line[absX])
+		}
+	}
+
+	return grid
 }
